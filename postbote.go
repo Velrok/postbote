07c09fc02c4c85a0b87717d6eb333078eb251505
Waldemar <waldemar.schwan@uswitch.com>
2016-10-06 16:33:15 +0100

    support stdin as input for messages

--- postbote.go
+++ postbote.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"github.com/streadway/amqp"
@@ -14,7 +15,7 @@ func failOnError(err error, msg string) {
 	}
 }
 
-func send(connectionStr string, messages chan []byte, exchange string) {
+func sendToRabbit(connectionStr string, messages chan []byte, exchange string) {
 	conn, err := amqp.Dial(connectionStr)
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
@@ -23,7 +24,8 @@ func send(connectionStr string, messages chan []byte, exchange string) {
 	failOnError(err, "Failed to open a channel")
 	defer ch.Close()
 
-	for {
+	for msg := range messages {
+		fmt.Fprintf(os.Stderr, "Trying to send %s... ", msg[:10])
 		err = ch.Publish(
 			exchange,
 			"",    // routing key
@@ -31,13 +33,14 @@ func send(connectionStr string, messages chan []byte, exchange string) {
 			false, // immediate
 			amqp.Publishing{
 				ContentType: "text/plain",
-				Body:        <-messages,
+				Body:        msg,
 			})
 		failOnError(err, "Failed to publish a message")
+		fmt.Fprintf(os.Stderr, " DONE\n")
 	}
 }
 
-func listen(connectionStr string, queue_name string, messages chan []byte) {
+func listenToRabbit(connectionStr string, queue_name string, messages chan []byte) {
 	conn, err := amqp.Dial(connectionStr)
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
@@ -67,20 +70,44 @@ func listen(connectionStr string, queue_name string, messages chan []byte) {
 
 func main() {
 
-	fromQueuePtr := flag.String("q", "", "The queue from wich to take the messages out of.")
+	fromQueuePtr := flag.String("q", "", "The queue from wich to take the messages out of. OR - to read from stdin.")
 	toExchangePtr := flag.String("x", "", "The exchange all the messages should be passed on to.")
 	connectionStrPtr := flag.String("rabbit", "amqp://guest:guest@localhost:5672/", "Rabbmit MQ connection string.")
 
 	flag.Parse()
 
+	fromStdIn := false
+	from := *fromQueuePtr
+	if *fromQueuePtr == "-" {
+		fromStdIn = true
+		from = "stdin"
+	}
+
 	fmt.Fprintf(os.Stderr, "Host: %s\n", *connectionStrPtr)
-	fmt.Fprintf(os.Stderr, "%s --->> %s\n", *fromQueuePtr, *toExchangePtr)
+	fmt.Fprintf(os.Stderr, "%s --->> %s\n", from, *toExchangePtr)
 
-	messages := make(chan []byte, 0)
-	go listen(*connectionStrPtr, *fromQueuePtr, messages)
-	go send(*connectionStrPtr, messages, *toExchangePtr)
+	messages := make(chan []byte)
 
+	go sendToRabbit(*connectionStrPtr, messages, *toExchangePtr)
+
+	if fromStdIn {
+		// processing file
+		fmt.Fprintf(os.Stderr, "Reading input from Stdin.\n")
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			messages <- []byte(scanner.Text())
+		}
+		if err := scanner.Err(); err != nil {
+			failOnError(err, "Failed reading Stdin.")
+		} else {
+			fmt.Fprintf(os.Stderr, "Done reading Stdin.\n")
+		}
+	} else {
+		// consuming rabbit
+		fmt.Fprintf(os.Stderr, "Listening for messages from RabbitMQ ...\n")
+		go listenToRabbit(*connectionStrPtr, *fromQueuePtr, messages)
+	}
 	forever := make(chan bool)
-	fmt.Fprintln(os.Stderr, "Listening for messages! Send Ctrl-C to stop...")
+	fmt.Fprintln(os.Stderr, "Send Ctrl-C to stop...")
 	<-forever
 }
